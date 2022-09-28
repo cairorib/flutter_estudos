3b20d34c8c9db1f49fc3fd3f940049052d618e72
cairo <cairoribeiro@hotmail.com>
2022-09-28 17:37:02 -0300

    btn gerencia de tema

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_todo/app_controller.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -9,14 +10,31 @@ class HomePage extends StatefulWidget {
 
 class HomePageState extends State<HomePage> {
   int counter = 0;
+
   @override
   Widget build(BuildContext context) {
     /* return Container(child: Center(child: Text('ALELUIA ALELUIA'))); */
     return Scaffold(
-      appBar: AppBar(
-        title: Text('ALELUIA!!!'),
-      ),
-      body: Center(
+        appBar: AppBar(
+          title: Text('ALELUIA!!!'),
+        ),
+        body: Container(
+          child: Switch(
+            value: AppController.instance.isDartTheme,
+            onChanged: (value) {
+              AppController.instance.changeTheme();
+              /* setState(() {
+                isDartTheme = value;
+              }); */
+            },
+          ),
+          /*  height: 200,
+          width: 200,
+          color: Colors.black,
+          child: Center(
+              child: Container(height: 100, width: 100, color: Colors.green)), */
+        )
+        /* Center(
         child: GestureDetector(
           child: Text(
             'clicados: $counter',
@@ -28,15 +46,15 @@ class HomePageState extends State<HomePage> {
             });
           },
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
+      ), */
+        /*  floatingActionButton: FloatingActionButton(
         onPressed: () {
           setState(() {
             counter++;
           });
         },
         child: Icon(Icons.add),
-      ),
-    );
+      ), */
+        );
   }
 }
