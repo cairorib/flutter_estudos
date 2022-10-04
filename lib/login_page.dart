ac1dd863b476b7da664d1b6fa40285525533cba1
cairo <cairoribeiro@hotmail.com>
2022-10-04 15:14:29 -0300

    input ok

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -7,22 +7,52 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   @override
+  String email = '';
+  String pswd = '';
+
   Widget build(BuildContext context) {
-    return Material(
+    return Scaffold(
+        body: SingleChildScrollView(
       child: SizedBox(
-          height: double.infinity,
-          width: double.infinity,
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
           child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 TextField(
-                  decoration: InputDecoration(border: OutlineInputBorder()),
+                  onChanged: (text) {
+                    email = text;
+                    /* print(text); */
+                  },
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                      labelText: 'Email', border: OutlineInputBorder()),
                 ),
+                SizedBox(height: 10),
+                TextField(
+                  onChanged: (text) {
+                    pswd = text;
+                    /* print(text); */
+                  },
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      labelText: 'Password', border: OutlineInputBorder()),
+                ),
+                SizedBox(height: 10),
+                ElevatedButton(
+                    onPressed: () {
+                      if (email == 'cairo@email' && pswd == 'teste123') {
+                        print('logado');
+                      } else {
+                        print('login errado');
+                      }
+                    },
+                    child: Text('Entrar'))
               ],
             ),
           )),
-    );
+    ));
   }
 }
